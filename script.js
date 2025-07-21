6c4e65a7f06cda9440a348b56c6b957c3ffedfd3
Luke Sawczak <luke@sawczak.com>
2025-07-21 12:21:40 -0400

    most of the way there

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-// Handy...
+/* Utilities */
 
+/**
+ * Add a rectangle of the given colour and coords (x, y, w, h) to the given surface.
+ * @param s
+ * @param colour
+ * @param coords
+ */
 const d = (s, colour, coords) => {
     colour = `#${colour}`;
     s.add(new DrawableRectangle(Rectangle.fromOriginAndDimensions(...coords), colour));
 }
 
+/**
+ * Return the offset for the ith block given the value of the block size
+ * and the division line size.
+ * @param i
+ * @param vb
+ * @param vd
+ * @returns {number}
+ */
 const ob = (i, vb, vd) => {
     return i * (vd + vb);
 }
 
+/**
+ * Return the offset for the ith division line given the value of the block size
+ * and the division line size.
+ * @param i
+ * @param vb
+ * @param vd
+ * @returns {*}
+ */
 const od = (i, vb, vd) => {
     return (i * (vd + vb)) + vb
 }
 
+/**
+ * Draw n blocks and division lines on the given surface.
+ * The last division line is omitted.
+ * @param s The surface
+ * @param n The number of blocks
+ * @param db The callback to draw the block
+ * @param dd The callback to draw the division (optional)
+ * @param adb Arguments to pass to the draw block callback (optional)
+ * @param add Arguments to pass to the draw division callback (optional)
+ */
 const seq = (s, n, db, dd, adb, add) => {
     if (typeof (adb) === 'undefined') {
         adb = [];
@@ -32,30 +64,50 @@ const seq = (s, n, db, dd, adb, add) => {
     db(s, n - 1, ...adb);
 }
 
-// Components
-
 /* Keys */
 
+/**
+ * Draw the ith white key on the given surface
+ * @param s
+ * @param i
+ */
 const draw_white = (s, i) => {
-    let y = ob(i, H_WHITE, H_WHITE_DIV)
-    d(s, C_WHITE, [0, y, W_WHITE, H_WHITE]);
+    let y = ob(i, p.H_WHITE, p.H_WHITE_DIV)
+    d(s, c.WHITE, [0, y, p.W_WHITE, p.H_WHITE]);
 }
 
+/**
+ * Draw the ith white key division line on the given surface
+ * @param s
+ * @param i
+ */
 const draw_white_div = (s, i) => {
-    let y = od(i, H_WHITE, H_WHITE_DIV);
-    d(s, C_WHITE_DIV, [0, y, W_WHITE, H_WHITE_DIV]);
+    let y = od(i, p.H_WHITE, p.H_WHITE_DIV);
+    d(s, c.WHITE_DIV, [0, y, p.W_WHITE, p.H_WHITE_DIV]);
 }
 
+/**
+ * Draw the ith black key on the given surface
+ * Each black key has a unique offset from its default y value,
+ * because black keys are not placed with 50/50 overlap on the two
+ * white keys they touch. This is determined by its index.
+ * @param s
+ * @param i
+ */
 const draw_black = (s, i) => {
-    if (YO_BLACK[i] === null) {
+    if (p.YO_BLACK[i] === null) {
         return;
     }
 
-    let yo = ((YO_BLACK[i] * H_BLACK) / 100);
-    let y = ob(i + 1, H_WHITE, H_WHITE_DIV) - yo;
-    d(s, C_BLACK, [0, y, W_BLACK, H_BLACK]);
+    let yo = ((p.YO_BLACK[i] * p.H_BLACK) / 100);
+    let y = ob(i + 1, p.H_WHITE, p.H_WHITE_DIV) - yo;
+    d(s, c.BLACK, [0, y, p.W_BLACK, p.H_BLACK]);
 }
 
+/**
+ * Draw piano keys on the given surface.
+ * @param s
+ */
 const draw_keys = (s) => {
     seq(s, 7, draw_white, draw_white_div);
     seq(s, 7, draw_black);
@@ -63,59 +115,107 @@ const draw_keys = (s) => {
 
 /* Measures */
 
+/**
+ * Draw the ith black row on the given measure.
+ * The height is the same as a black key.
+ * The y offset works the same way as the corresponding black key
+ * (in draw_black).
+ * @param s
+ * @param i
+ */
 const draw_row_black = (s, i) => {
-    if (YO_BLACK[i] === null) {
+    if (p.YO_BLACK[i] === null) {
         return;
     }
 
-    let yo = ((YO_BLACK[i] * H_BLACK) / 100);
-    let y = ob(i + 1, H_WHITE, H_WHITE_DIV) - yo;
-    d(s, C_ROW_BLACK, [0, y, W_MEASURE, H_BLACK]);
-    seq(s, N_BEATS_PER_MEASURE, draw_beat, draw_beat_div_black, [], [y]);
+    let yo = ((p.YO_BLACK[i] * p.H_BLACK) / 100);
+    let y = ob(i + 1, p.H_WHITE, p.H_WHITE_DIV) - yo;
+    d(s, c.ROW_BLACK, [0, y, p.W_MEASURE, p.H_BLACK]);
+    seq(s, p.N_BEATS_PER_MEASURE, draw_beat, draw_beat_div_black, [], [y]);
 }
 
+/**
+ * Draw the ith beat on the given measure.
+ * However, "beats" are just the voids between the beat division lines,
+ * so this is a dummy function.
+ * @param s
+ * @param i
+ */
 const draw_beat = (s, i) => {}
 
+/**
+ * Draw the ith beat division line on the given surface.
+ * This is on the white background.
+ * @param s
+ * @param i
+ */
 const draw_beat_div_white = (s, i) => {
-    let x = od(i, W_BEAT, W_BEAT_DIV)
-    d(s, C_BEAT_DIV_WHITE, [x, 0, W_BEAT_DIV, H_OCTAVE]);
+    let x = od(i, p.W_BEAT, p.W_BEAT_DIV)
+    d(s, c.BEAT_DIV_WHITE, [x, 0, p.W_BEAT_DIV, p.H_OCTAVE]);
 }
 
+/**
+ * Draw the ith beat division line on the given surface.
+ * This is on the black background.
+ * @param s
+ * @param i
+ */
 const draw_beat_div_black = (s, i, y) => {
-    let x = od(i, W_BEAT, W_BEAT_DIV);
-    d(s, C_BEAT_DIV_BLACK, [x, y, W_BEAT_DIV, H_BLACK]);
+    let x = od(i, p.W_BEAT, p.W_BEAT_DIV);
+    d(s, c.BEAT_DIV_BLACK, [x, y, p.W_BEAT_DIV, p.H_BLACK]);
 }
 
+/**
+ * Draw the ith measure on the given surface.
+ * A measure is white rows, black rows, and beat division lines crossing them.
+ * @param s
+ * @param i
+ */
 const draw_measure = (s, i) => {
-    let x = ob(i, W_MEASURE, W_MEASURE_DIV);
+    let x = ob(i, p.W_MEASURE, p.W_MEASURE_DIV);
     let me = new Subcontext([x, 0]);
 
-    d(me, C_ROW_WHITE, [0, 0, W_MEASURE, H_OCTAVE]);
-    d(me, C_ROW_WHITE_DIV, [0, od(3, H_WHITE, H_WHITE_DIV), W_MEASURE, H_WHITE_DIV]);
-    seq(me, N_BEATS_PER_MEASURE, draw_beat, draw_beat_div_white);
+    d(me, c.ROW_WHITE, [0, 0, p.W_MEASURE, p.H_OCTAVE]);
+    d(me, c.ROW_WHITE_DIV, [0, od(3, p.H_WHITE, p.H_WHITE_DIV), p.W_MEASURE, p.H_WHITE_DIV]);
+    seq(me, p.N_BEATS_PER_MEASURE, draw_beat, draw_beat_div_white);
     seq(me, 7, draw_row_black);
 
     s.add(me);
 }
 
+/**
+ * Draw the ith measure division line on the given surface.
+ * @param s
+ * @param i
+ */
 const draw_measure_div = (s, i) => {
-    let x = od(i, W_MEASURE, W_MEASURE_DIV);
-    d(s, C_MEASURE_DIV_BLACK, [x, 0, W_MEASURE_DIV, H_OCTAVE]);
+    let x = od(i, p.W_MEASURE, p.W_MEASURE_DIV);
+    d(s, c.MEASURE_DIV_BLACK, [x, 0, p.W_MEASURE_DIV, p.H_OCTAVE]);
 }
 
+/**
+ * Draw measures on the given surface.
+ * @param s
+ */
 const draw_measures = (s) => {
-    seq(s, N_MEASURES, draw_measure, draw_measure_div);
+    seq(s, p.N_MEASURES, draw_measure, draw_measure_div);
 }
 
 /* Octaves */
 
+/**
+ * Draw the ith octave on the given surface.
+ * An octave is the keys on the left and the measures on the right.
+ * @param s
+ * @param i
+ */
 const draw_octave = (s, i) => {
-    let y = ob(i, H_OCTAVE, H_OCTAVE_DIV);
-    let me = new Subcontext([W_L_BAR, y]);
+    let y = ob(i, p.H_OCTAVE, p.H_OCTAVE_DIV);
+    let me = new Subcontext([p.W_L_BAR, y]);
 
     let sKeys = new Subcontext([0, 0]);
 
-    let xMeasures = (W_WHITE + W_R_BAR_1 + W_R_BAR_2);
+    let xMeasures = (p.W_WHITE + p.W_R_BAR_1 + p.W_R_BAR_2);
     let sMeasures = new Subcontext([xMeasures, 0]);
 
     draw_keys(sKeys);
@@ -126,14 +226,23 @@ const draw_octave = (s, i) => {
     s.add(me);
 }
 
+/**
+ * Draw the ith octave division line on the given surface.
+ * The octave division line crosses both the keys and the measures.
+ * @param s
+ * @param i
+ */
 const draw_octave_div = (s, i) => {
-    let y = od(i, H_OCTAVE, H_OCTAVE_DIV);
-    d(s, C_OCTAVE_DIV, [W_L_BAR, y, W_WHITE, H_OCTAVE_DIV]);
-    d(s, C_ROW_OCTAVE_DIV, [(W_L_BAR + W_WHITE + W_R_BAR_1 + W_R_BAR_2), y, W, H_OCTAVE_DIV]);
+    let y = od(i, p.H_OCTAVE, p.H_OCTAVE_DIV);
+    d(s, c.OCTAVE_DIV, [p.W_L_BAR, y, p.W_WHITE, p.H_OCTAVE_DIV]);
+    d(s, c.ROW_OCTAVE_DIV, [(p.W_L_BAR + p.W_WHITE + p.W_R_BAR_1 + p.W_R_BAR_2), y, p.W, p.H_OCTAVE_DIV]);
 }
 
 /* Main */
 
+/**
+ * Reset the canvas and its variables.
+ */
 const reset = () => {
     let wrap = $('#canvasKeysWrap');
     let w = wrap.width();
@@ -145,33 +254,48 @@ const reset = () => {
 
     canvas = $('#canvasKeys').get(0);
     ctx = canvas.getContext('2d');
+    p = deriveParameters();
 }
 
+/**
+ * Draw the canvas.
+ */
 const draw = () => {
     reset();
 
     sub = new Subcontext([0, 0]);
-    // d(sub, C_BG, [0, 0, W, H]);
+    // d(sub, BG, [0, 0, W, H]); // Background colour
 
-    seq(sub, N_OCTAVES, draw_octave, draw_octave_div);
+    seq(sub, p.N_OCTAVES, draw_octave, draw_octave_div);
 
-    d(sub, C_L_BAR, [0, 0, W_L_BAR, H]);
-    d(sub, C_R_BAR_1, [W_L_BAR + W_WHITE, 0, W_R_BAR_1, H]);
-    d(sub, C_R_BAR_2_WHITE, [W_L_BAR + W_WHITE + W_R_BAR_1, 0, W_R_BAR_2, H]);
+    d(sub, c.L_BAR, [0, 0, p.W_L_BAR, p.H]);
+    d(sub, c.R_BAR_1, [p.W_L_BAR + p.W_WHITE, 0, p.W_R_BAR_1, p.H]);
+    d(sub, c.R_BAR_2_WHITE, [p.W_L_BAR + p.W_WHITE + p.W_R_BAR_1, 0, p.W_R_BAR_2, p.H]);
 
     sub.draw(ctx);
 }
 
+/**
+ * Bind the controls.
+ */
 const bind = () => {
-
+    $('.controlInput').change(draw);
 }
 
+/**
+ * Initialize the app.
+ */
 const initialize = () => {
     draw();
     bind();
 }
 
+/**
+ * Actual on load stuff.
+ */
 let canvas;
 let ctx;
 let sub;
+let p; // Parameters
+const c = colours; // Convenience
 $(document).ready(initialize);
--- settings.js
+++ settings.js
@@ -1,46 +1,83 @@
-var N_BEATS_PER_MEASURE = 16
-var N_MEASURES = 3
-var N_OCTAVES = 3
+const defaultBasics = {
+    N_BEATS_PER_MEASURE: 16,
+    N_MEASURES: 3,
+    N_OCTAVES: 3,
 
-var H_SCALE = 2
-var V_SCALE = 2.65
+    W_SCALE_FACTOR: 2,
+    H_SCALE_FACTOR: 2.65,
+    W_SCALE: 1,
+    H_SCALE: 1,
 
-var W_L_BAR = H_SCALE * 1
-var W_R_BAR_1 = H_SCALE * 3
-var W_R_BAR_2 = H_SCALE * 1
-var W_WHITE = H_SCALE * 191
-var W_BLACK = H_SCALE * 109
-var W_BEAT = H_SCALE * 10
-var W_BEAT_DIV = H_SCALE * 1
-var W_MEASURE = ((N_BEATS_PER_MEASURE * (W_BEAT + W_BEAT_DIV)) - W_BEAT_DIV)
-var W_MEASURE_DIV = H_SCALE * 1
+    YO_BLACK: [70, 50, 30, null, 60, 40, null]
+}
 
-var H_WHITE = V_SCALE * 25
-var H_WHITE_DIV = V_SCALE * 1
-var H_BLACK = V_SCALE * 15
-var H_OCTAVE_DIV = V_SCALE * 1
-var H_OCTAVE = ((7 * (H_WHITE + H_WHITE_DIV)) - H_WHITE_DIV)
+const defaultWidths = {
+    W_L_BAR: 1,
+    W_R_BAR_1: 3,
+    W_R_BAR_2: 1,
+    W_WHITE: 191,
+    W_BLACK: 109,
+    W_BEAT: 10,
+    W_BEAT_DIV: 1,
+    W_MEASURE_DIV: 1
+}
 
-var YO_BLACK = [70, 50, 30, null, 60, 40, null]
+const defaultHeights = {
+    H_WHITE: 25,
+    H_WHITE_DIV: 1,
+    H_BLACK: 15,
+    H_OCTAVE_DIV: 1
+}
 
-var C_BG = '000000'
-var C_WHITE = 'ffffff'
-var C_BLACK = '000000'
-var C_L_BAR = 'abb1b1'
-var C_R_BAR_1 = 'abb1b1'
-var C_R_BAR_2_WHITE = '5c5c5c'
-var C_R_BAR_2_BLACK = '555555'
-var C_WHITE_DIV = '5d5d5d'
-var C_ROW_WHITE = '464646'
-var C_ROW_BLACK = '3e3e3e'
-var C_ROW_WHITE_DIV = '3e3e3e'
-var C_ROW_OCTAVE_DIV = '383838'
-var C_OCTAVE_DIV = '111111'
-var C_WHITE_DIV_BEAT_DIV = '363636'
-var C_BEAT_DIV_BLACK = '363636'
-var C_BEAT_DIV_WHITE = '3d3d3d'
-var C_MEASURE_DIV_WHITE = '383838'
-var C_MEASURE_DIV_BLACK = '313131'
+const colours = {
+    BG: '000000',
+    WHITE: 'ffffff',
+    BLACK: '000000',
+    L_BAR: 'abb1b1',
+    R_BAR_1: 'abb1b1',
+    R_BAR_2_WHITE: '5c5c5c',
+    R_BAR_2_BLACK: '555555',
+    WHITE_DIV: '5d5d5d',
+    ROW_WHITE: '464646',
+    ROW_BLACK: '3e3e3e',
+    ROW_WHITE_DIV: '3e3e3e',
+    ROW_OCTAVE_DIV: '383838',
+    OCTAVE_DIV: '111111',
+    WHITE_DIV_BEAT_DIV: '363636',
+    BEAT_DIV_BLACK: '363636',
+    BEAT_DIV_WHITE: '3d3d3d',
+    MEASURE_DIV_WHITE: '383838',
+    MEASURE_DIV_BLACK: '313131',
+}
 
-var W = (W_L_BAR + W_WHITE + W_R_BAR_1 + W_R_BAR_2 + ((N_MEASURES * (W_MEASURE + W_MEASURE_DIV)) - W_MEASURE_DIV))
-var H = (N_OCTAVES * (H_OCTAVE + H_OCTAVE_DIV)) - H_OCTAVE_DIV
+const deriveParameters = () => {
+    // Copy parameters and update from inputs
+    let s = JSON.parse(JSON.stringify(defaultBasics));
+    s.W_SCALE = parseFloat($('#inputWScale').val());
+    s.H_SCALE = parseFloat($('#inputHScale').val());
+
+    let hs = JSON.parse(JSON.stringify(defaultHeights));
+    hs.H_WHITE = parseFloat($('#inputHWhite').val());
+    hs.H_BLACK = parseFloat($('#inputHBlack').val());
+
+    let ws = JSON.parse(JSON.stringify(defaultWidths));
+
+    // Scale everything
+    Object.keys(ws).forEach(function(k, i) {
+        ws[k] *= s.W_SCALE * s.W_SCALE_FACTOR;
+    });
+
+    Object.keys(hs).forEach(function(k, i) {
+        hs[k] *= s.H_SCALE * s.H_SCALE_FACTOR;
+    });
+
+    s = {...s, ...ws, ...hs};
+
+    // Derive convenience parameters
+    s.W_MEASURE = ((s.N_BEATS_PER_MEASURE * (s.W_BEAT + s.W_BEAT_DIV)) - s.W_BEAT_DIV);
+    s.H_OCTAVE = ((7 * (s.H_WHITE + s.H_WHITE_DIV)) - s.H_WHITE_DIV);
+    s.W = (s.W_L_BAR + s.W_WHITE + s.W_R_BAR_1 + s.W_R_BAR_2 + ((s.N_MEASURES * (s.W_MEASURE + s.W_MEASURE_DIV)) - s.W_MEASURE_DIV));
+    s.H = (s.N_OCTAVES * (s.H_OCTAVE + s.H_OCTAVE_DIV)) - s.H_OCTAVE_DIV;
+
+    return s;
+}
